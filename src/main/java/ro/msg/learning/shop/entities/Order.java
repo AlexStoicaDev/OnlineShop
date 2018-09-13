a3822c4811de7e709f8d76a6fcc227f4b7f0f478
Spetrut <petrutstoica123@yahoo.com>
2018-09-13 09:53:46 +0300

    data model done, all entities and repositories done, project structure done

---
+++ src/main/java/ro/msg/learning/shop/entities/Order.java
@@ -0,0 +1,34 @@
+package ro.msg.learning.shop.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "orders")
+public class Order {
+
+    @ManyToOne
+    private Customer customer;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "order")
+    private List<OrderDetail> orderDetails;
+
+
+    @ManyToMany
+    @JoinTable(name = "orders_location",
+        joinColumns = @JoinColumn(name = "order_id"),
+        inverseJoinColumns = @JoinColumn(name = "location_id"))
+    private List<Location> locations;
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+}
---
+++ src/main/java/ro/msg/learning/shop/entities/OrderDetail.java
@@ -0,0 +1,30 @@
+package ro.msg.learning.shop.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class OrderDetail {
+
+
+    @ManyToOne
+    private Product product;
+    @ManyToOne
+    private Order order;
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private Integer quantity;
+
+
+}
